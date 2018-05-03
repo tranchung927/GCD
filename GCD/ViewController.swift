71aee5a15cf253ba421adf3978ce79c14c9223fc
ChungTV <tranchung927@gmail.com>
2018-05-03 15:28:34 +0700

    sync tasks in serial queue

--- GCD/ViewController.swift
+++ GCD/ViewController.swift
@@ -13,13 +13,32 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        simpleQueue()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func simpleQueue() {
+        
+        // Khai báo serial queue
+        let serialQueue = DispatchQueue(label: "com.bigZero.serialQueue")
+        
+        // Vòng lặp 5 lần tượng trưng đưa 5 task vào trong serial queue
+        for i in 1...5 {
+            
+            // Các task đưa vào sẽ được đánh dấu là Synchronous
+            serialQueue.sync {
+                print("Task \(i): - \(Thread.current))")
+                // Vì hệ thống sẽ xử ý rất nhanh
+                //nên mỗi lần chạy xong nghỉ 1s để quan sát rõ hơn.
+                sleep(1)
+            }
+        }
+        print("Complete! - \(Thread.current)")
+    }
 
 }
 
